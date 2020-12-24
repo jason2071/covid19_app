35e4eef07ccd48f7f808802bf1500904eb36e7c9
Phumai <mcc3939@gmail.com>
2020-12-24 13:56:53 +0700

    add font and setting app

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFE9901);
+const kSecondaryColor = Color(0xFF979797);
+const kBackgroundColor = Color(0xFFF2F2F2);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+final kDefaultShadow = BoxShadow(
+  spreadRadius: 1.0,
+  blurRadius: 10,
+  color: Colors.black12,
+);
---
+++ lib/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    fontFamily: "Kanit",
+    appBarTheme: appBarTheme(),
+    primaryColor: kPrimaryColor,
+    accentColor: kPrimaryColor,
+    canvasColor: Colors.transparent,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return AppBarTheme(
+    color: kPrimaryColor,
+    elevation: 0,
+    brightness: Brightness.light,
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.white),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+  );
+}
